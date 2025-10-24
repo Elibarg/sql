0e0bf67b25f26793051e49f13e84e7445d29bc2e
Elibarg <calagabriel31@gmail.com>
2025-10-23 21:00:19 -0300

    first commit

---
+++ ot_1/mercado.sql
@@ -0,0 +1,13 @@
+create database Mercado
+
+use mercado;
+create table categoria (
+idcategoria int NOT NULL primary KEY auto_increment,
+ decricao varchar(45);
+);
+
+insert into categoria values(1,'higiene pessoal');
+insert into categoria values(2,'limpeza');
+insert into categoria values(3,'cama, mesa, banho');
+insert into categoria values(4,'frios');
+insert into categoria values(5,'bazar');
